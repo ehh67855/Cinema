238c376e8a7e6e76568e4c7fb00891c1742123e3
Firedynex <58797454+Firedynex@users.noreply.github.com>
2024-04-26 16:46:51 -0400

    Added ability to select theater in frontend

--- frontend/src/admin/AdminEditTicketsPage.js
+++ frontend/src/admin/AdminEditTicketsPage.js
@@ -7,9 +7,10 @@ export default function AdminEditPricingPage() {
     let adultTicketPrice = useRef();
     let childTicketPrice = useRef();
     let bookingFees = useRef();
+    let theaterSelection = useRef();
 
     useEffect(() => {
-        
+
     })
 
     return (
@@ -19,13 +20,21 @@ export default function AdminEditPricingPage() {
             </div>
             <form id="pricingInputForm">
                 <div className="inputFields">
-                    <label>Senior Ticket Price: </label>
+                    <label for="theaterSelector">Theater</label>
+                    <select name="theaterSelector" id="theaterSelector" ref={theaterSelection}>
+                        <option value="1">Theater 1</option>
+                        <option value="2">Theater 2</option>
+                        <option value="3">Theater 3</option>
+                        <option value="4">Theater 4</option>
+                        <option value="5">Theater 5</option>
+                    </select>
+                    <label for="seniorTicketPrice">Senior Ticket Price: </label>
                     <input type="number" min="0" id="seniorTicketPrice" ref={seniorTicketPrice}></input>
-                    <label>Adult Ticket Price: </label>
+                    <label for="adultTicketPrice">Adult Ticket Price: </label>
                     <input type="number" min="0" id="adultTicketPrice" ref={adultTicketPrice}></input>
-                    <label>Child Ticket price: </label>
+                    <label for="childTicketPrice">Child Ticket price: </label>
                     <input type="number" min="0" id="childTicketPrice" ref={childTicketPrice}></input>
-                    <label>Booking Fees: </label>
+                    <label for="bookingFees">Booking Fees: </label>
                     <input type="number" min="0" id="bookingFees" ref={bookingFees}></input> <br/>
                     <button id="submitButton">Update Pricing</button> <br/>
                 </div>
