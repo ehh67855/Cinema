bcc307ea40c218bee06a16f8afbf72a334a7cbb9
rrj03260 <rrj03260@uga.edu>
2024-04-29 22:42:23 -0400

    button for edit ticket pricing page and css updates
    The admin main page looks a little better, and there's now a button that takes the admin to AdminEditTicketPage.

--- frontend/src/admin/AdminMainPage.js
+++ frontend/src/admin/AdminMainPage.js
@@ -8,6 +8,7 @@ const AdminMainPage = () => {
             <Link to={`/manageMovies`} className="adminMainPageBtn"><button>Manage Movies</button></Link>
             <Link to={`/manageUsers`} className="adminMainPageBtn"><button>Manage Users</button></Link>
             <Link to={`/promotions`} className="adminMainPageBtn"><button>Manage Promotions and fees</button></Link>
+            <Link to={`/editPricing`} className="adminMainPageBtn"><button>Manage Ticket Prices</button></Link>
         </div>
     );
 }
